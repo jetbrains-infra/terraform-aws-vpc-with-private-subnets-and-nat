079226e6194430b9e8fd22dfee87edd8d3bc5225
Sergey Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2018-06-13 10:55:39 +0300

    Params for GW and instance types of NAT

--- variables.tf
+++ variables.tf
@@ -5,3 +5,15 @@ variable "multi_az" {
 variable "rds" {
   default = false
 }
+variable "nat_mode" {
+  description = "Could be 'gateway' or 'instance', read more: https://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/vpc-nat-comparison.html"
+  default = "gateway"
+}
+
+locals {
+  nat_instance          = "${ var.nat_mode == "gateway" ? false : true }"
+  nat_instance_multi_az = "${ local.nat_instance == false ? false : var.multi_az }"
+
+  nat_gw                = "${ var.nat_mode == "gateway" ? true : false }"
+  nat_gw_multi_az       = "${ local.nat_gw == false ? false : var.multi_az }"
+}
