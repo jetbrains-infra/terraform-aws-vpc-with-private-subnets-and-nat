7a3a5cb93b3d53390026f39ff56ec0e3a9e8c35f
Sergey Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2018-07-07 16:41:16 +0300

    Allow access to internet on public subnet #2

--- internet_gateway.tf
+++ internet_gateway.tf
@@ -26,11 +26,10 @@ resource "aws_route_table" "public_2_to_internet" {
   }
 
   tags {
-    Name = "Nat #2 to Internet"
+    Name = "Public net #2 to Internet"
   }
 }
 resource "aws_route_table_association" "internet_for_public_2" {
-  count = "${var.multi_az ? 1 : 0}"
   route_table_id = "${aws_route_table.public_2_to_internet.id}"
   subnet_id = "${aws_subnet.public_nat_2.id}"
 }
